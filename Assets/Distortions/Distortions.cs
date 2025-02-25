9f20958e1bb40602fac4a35972b403df1667e245
Yannick Sauer <yannick.s204@gmail.com>
2025-02-25 17:16:42 +0100

    Example Unity scene: office; DIstortion simulation included.

---
+++ Assets/Distortions/Distortions.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Distortions : MonoBehaviour
+{
+    // settings
+    public bool active = false;
+    public float multiplier = 1.0f;
+    [Range(-50.0f, 50.0f)]
+    public float displacementX = 0.0f; // displacelemnt of simulated lens center in degrees of visual field
+    [Range(-50.0f, 50.0f)]
+    public float displacementY = 0.0f; // displacelemnt of simulated lens center in degrees of visual field
+                                       //public Shader ParametricDistortion;
+    private Material distortionMaterial;
+    private static Camera cam;
+    [Range(0.5f, 10.0f)]
+    public float magn = 1.0f;
+    [Range(-0.5f, 0.5f)]
+    public float radial = 0.0f;
+    [Range(-0.5f, 0.5f)]
+    public float asym = 0.0f;
+
+    void Awake()
+    {
+        distortionMaterial = new Material(Shader.Find("Hidden/RadialDistortion"));
+        // calculate FoV and scaling factors for transformation
+        cam = gameObject.GetComponent<Camera>();
+        print("Vertical FoV: " + cam.fieldOfView);
+        print("Horizontal FoV: " + cam.fieldOfView * cam.aspect);
+        float scalingX = multiplier * Mathf.Tan(Mathf.Deg2Rad * (cam.fieldOfView * cam.aspect / 2.0f));
+        float scalingY = multiplier * Mathf.Tan(Mathf.Deg2Rad * (cam.fieldOfView / 2.0f));
+        distortionMaterial.SetFloat("_XScaling", scalingX);
+        distortionMaterial.SetFloat("_YScaling", scalingY);
+        distortionMaterial.SetFloat("_XShift", Mathf.Tan(Mathf.Deg2Rad * displacementX));
+        distortionMaterial.SetFloat("_YShift", Mathf.Tan(Mathf.Deg2Rad * displacementY));
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (active)
+        {
+            distortionMaterial.SetFloat("_XShift", Mathf.Tan(Mathf.Deg2Rad * displacementX));
+            distortionMaterial.SetFloat("_YShift", Mathf.Tan(Mathf.Deg2Rad * displacementY));
+            distortionMaterial.SetFloat("_radial", radial);
+            distortionMaterial.SetFloat("_magn", magn);
+            distortionMaterial.SetFloat("_asym", asym);
+
+            Graphics.Blit(source, destination, distortionMaterial);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
---
+++ Assets/Distortions/RadialDistortion.shader
@@ -0,0 +1,87 @@
+﻿Shader "Hidden/RadialDistortion"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    
+    // Shader code pasted into all further CGPROGRAM blocks
+    CGINCLUDE
+    #include "UnityCG.cginc"
+
+    struct appdata
+    {
+       float4 vertex : POSITION;
+       float2 uv : TEXCOORD0;
+    };
+
+    
+
+    struct v2f
+    {
+        float2 uv : TEXCOORD0;
+        float4 vertex : SV_POSITION;
+    };
+
+    sampler2D _MainTex;
+    
+    float _magn;
+    float _radial;
+    float _asym;
+    float _XScaling;
+    float _YScaling;
+    float _XShift;
+    float _YShift;
+    float4 _MainTex_ST;
+    
+    fixed4 frag (v2f input) : SV_Target
+    {
+        float2 uv = input.uv;
+        float2 uv_distorted;
+		float2 xy = (uv - 0.5) * 2;
+		// get the vertical and horizontal shift of the projection matrix to correct for the shifted center of the texture
+		float x_shift = unity_CameraProjection[0][2]; 
+		float y_shift = unity_CameraProjection[1][2];
+		
+        xy.x = xy.x + x_shift;
+		xy.y = xy.y + y_shift;
+        
+        float r_sq = xy.x* xy.x + (xy.y - _asym) * (xy.y - _asym);
+        
+        //xd = (_magn + _radial * r) * x;
+        //yd = (_magn + _radial * r) * y;
+
+        uv_distorted.x =  1 / (_magn + _radial * r_sq) * xy.x;
+        uv_distorted.y =  1/(_magn + _radial * r_sq) * xy.y;
+        
+        // reverse the shift and scaling back to [0,1]
+		uv_distorted.x = uv_distorted.x - x_shift;
+		uv_distorted.y = uv_distorted.y - y_shift;
+		uv_distorted = uv_distorted / 2 + 0.5;
+
+        //UnityStereoScreenSpaceUVAdjust(uv_distorted, _MainTex_ST);
+        return tex2D(_MainTex, uv_distorted);
+    }
+    
+    v2f vert (appdata v)
+    {
+        v2f o;
+        o.vertex = UnityObjectToClipPos(v.vertex);
+        o.uv = v.uv;
+        return o;
+    }
+    ENDCG
+
+    SubShader
+    {
+        Pass
+        {
+            // No culling or depth
+            Cull Off ZWrite Off ZTest Always
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            ENDCG
+        }
+    }
+}
