c42757a1e747e2a4a9420696c7bd534af235f310
Yannick Sauer <yannick.s204@gmail.com>
2025-04-30 14:03:44 +0200

    Added v0.1 gun.

---
+++ Assets/GunController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // perform raycast in direction of the gun and show a point at the hit position
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+            // check if the hit object is a balloon
+            Balloon balloon = hit.collider.GetComponent<Balloon>();
+            if (balloon != null)
+            {
+                // pop the balloon
+                balloon.Pop();
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+
+
+    
+    private void Shoot()
+    {
+    
+    }
+
+}
